1a311b5c0a3bacb52c4ed89b62f6f04a6cd48573
Christian Nunciato <cnunciato@opscode.com>
2014-03-14 22:59:27 -0700

    Add a first test

--- spec/omniauth/strategies/chef_oauth2_spec.rb
+++ spec/omniauth/strategies/chef_oauth2_spec.rb
@@ -1,7 +1,11 @@
 # encoding: utf-8
-
 require 'spec_helper'
 
 describe OmniAuth::Strategies::ChefOAuth2 do
-  # Things
+  subject { OmniAuth::Strategies::ChefOAuth2.new(nil) }
+
+  it 'should camelize properly' do
+    expect(OmniAuth::Utils.camelize('chef_oauth2')).to eq('ChefOAuth2')
+  end
+
 end
